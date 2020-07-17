7fdbeb4a9535c150ca0c5f504a721d177fb09bbe
jtdorsey22 <jonathandorsey1117@gmail.com>
2020-07-16 21:59:30 -0700

    new stuffs

--- K_Armed_Bandit/BaseBandit.m
+++ K_Armed_Bandit/BaseBandit.m
@@ -3,34 +3,48 @@ classdef BaseBandit
     %   Detailed explanation goes here
     
     properties
-        k_arm = 10; 
-        eps = .1; 
-        Q = zeros(k_arm); 
-        action_space = [1:1:k_arm];  
+        k_arm
+        eps  
+        Q  
+        action_space   
         
     end
     
     methods
-        function  reset(inputArg1,inputArg2)
-           
-        end
-        
-        function action = act(obj,inputArg)
-           action;
-        end
-        
-        function r = reward()
-            r;
-        end 
-        
-        function update_Q()
-           obj.Q = Q;
-        end 
-        
-        function [action, reward] = step()
+        function obj = BaseBandit(num_arm, epsilon)
+            obj.k_arm = num_arm;
+            obj.eps = epsilon;  
+            Q = zeros(obj.k_arm); 
+            action_space = [1:1:obj.k_arm]; 
             
         end 
         
+        function print_things(obj, string_print)
+        
+            disp(string_print)
+        
+        end
+        
+%         function  reset(inputArg1,inputArg2)
+%            
+%         end
+%         
+%         function action = act(obj,inputArg)
+%            action;
+%         end
+%         
+%         function r = reward()
+%             r;
+%         end 
+%         
+%         function update_Q()
+%            obj.Q = Q;
+%         end 
+%         
+%         function [action, reward] = step()
+%             
+%         end 
+%         
         
     end
 end
